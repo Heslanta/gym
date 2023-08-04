3371d4bec54baa682b3d34b412813154c466798d
Charvia <90585985+Heslanta@users.noreply.github.com>
2023-08-04 10:39:39 +0800

    add new code

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/banner.png";
+import { Block } from "@mui/icons-material";
 
 const HeroBanner = () => {
   return (
@@ -8,14 +11,37 @@ const HeroBanner = () => {
         mt: { lg: "212px", xs: "70px" },
         ml: { sm: "50px" },
       }}
+      position="relative"
+      p="20px"
     >
       <Typography color="#ff2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
-      <Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+      >
         Sweat, Smile <br /> and Repeat
       </Typography>
-      <Typography>Check out the most effective exercises</Typography>
+      <Typography fontSize="22px" lineHeight="35px" mb={2}>
+        Check out the most effective exercises personalized to you
+      </Typography>
+      <Button variant="contained" color="error" href="#exercises">
+        Explore Exercises
+      </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{
+          opacity: 0.1,
+          display: { lg: "block", xs: "none" },
+        }}
+        mt={-6}
+        fontSize="200px"
+      >
+        Exercises
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   );
 };
---
+++ src/utils/fetchData.js
@@ -0,0 +1,20 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+try {
+  const response = await axios.request(options);
+  console.log(response.data);
+} catch (error) {
+  console.error(error);
+}
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
